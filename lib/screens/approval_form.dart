4f8e729d35cedde6848e7f689f1fb4b4dacb917c
MKSYAHMI7 <mksyahmiof@gmail.com>
2020-01-03 20:55:51 +0800

    5th commit

---
+++ lib/screens/approval_form.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:offleaveppkt/screens/notifications/note_list.dart';
+import 'package:offleaveppkt/model/leaveData.dart';
+import 'package:offleaveppkt/services/database.dart';
+import 'package:provider/provider.dart';
+
+
+class Approval extends StatefulWidget {
+
+final String title;
+Approval(this.title);
+
+  @override
+  _ApprovalState createState() => _ApprovalState();
+}
+
+class _ApprovalState extends State<Approval> {
+  @override
+  Widget build(BuildContext context) {
+
+    return StreamProvider<List<LeaveData>>.value(
+      value: DatabaseService2().leaveData,
+
+      child: Scaffold(
+        backgroundColor: Colors.red[400],
+        appBar: AppBar(
+          title: Text('Approval notifications'),
+          backgroundColor: Colors.green[500],
+          elevation: 0.0,
+        ),
+
+        body: Container(
+          child: FilledList(),
+
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/screens/notifications/note_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:offleaveppkt/model/leaveData.dart';
+import 'note_tile.dart';
+
+
+
+class FilledList extends StatefulWidget {
+  @override
+  _FilledListState createState() => _FilledListState();
+}
+
+class _FilledListState extends State<FilledList> {
+  @override
+  Widget build(BuildContext context) {
+
+   final leaveData = Provider.of<List<LeaveData>>(context) ?? []; //empty array
+
+    return ListView.builder(
+      itemCount: leaveData.length,
+      itemBuilder: (context, index){
+        return NoteTile(leaveData: leaveData[index]);
+      },
+    );
+
+  }
+
+}
